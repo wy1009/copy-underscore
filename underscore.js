7e69a115aeec968f5f64a6d7710ee738a2a6683c
wangyi <wy941009@gmail.com>
2017-12-12 19:22:42 +0800

    typo

--- underscore.js
+++ underscore.js
@@ -698,6 +698,6 @@
             max = min
             min = 0
         }
-        return Math.floor(Math.ramdom() * (max - min + 1)) + min
+        return Math.floor(Math.random() * (max - min + 1)) + min
     }
 })()
