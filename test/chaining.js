3c9c3ebb9b88684ed14cb77b247f589175cf1ec0
wangyi <wy941009@gmail.com>
2017-12-14 17:07:47 +0800

    chain，增加第二种调用方式的测试用例

--- test/chaining.js
+++ test/chaining.js
@@ -6,7 +6,7 @@
             'He\'s a lumberjack and he\'s okay',
             'He sleeps all night and he works all day'
         ]
-        var counts = _.chain(lyrics)
+        var counts = _(lyrics).chain()
             .map(function (line) { return line.split('') })
             .flatten()
             .reduce(function (hash, l) {
@@ -15,7 +15,17 @@
                 return hash
             }, {})
             .value()
-        assert.strictEqual(counts.a, 16, 'counted all the letters in the song')
-        assert.strictEqual(counts.e, 10, 'counted all the letters in the song')
+        assert.strictEqual(counts.a, 16)
+        assert.strictEqual(counts.e, 10)
+        var counts = _.chain(lyrics)
+        .map(function (line) { return line.split('') })
+        .flatten()
+        .reduce(function (hash, l) {
+            hash[l] = hash[l] || 0
+            hash[l]++
+            return hash
+        }, {})
+        .value()
+        assert.strictEqual(counts.a, 16, '支持两种调用方式')
     })
 })()
